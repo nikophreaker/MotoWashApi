c70c74044c7ef5ea2c72a882be5b90706058ceae
Nikxphreaker <nikx449@gmail.com>
2023-10-08 12:50:24 +0700

    edit middleware

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -3,10 +3,14 @@ import { getServerSession } from '#auth'
 
 export default eventHandler(async (event) => {
     const session = await getServerSession(event)
-    if (event._path?.includes("login") || event._path?.includes("auth")) {
+    if (event._path?.includes("auth")) {
         return
     }
-    if (!session) {
-        throw createError({ statusMessage: 'Unauthenticated', statusCode: 403 })
+    if (event._path?.includes("api")) {
+        if (!session) {
+            throw createError({ statusMessage: 'Unauthenticated', statusCode: 403 })
+        } else {
+            return
+        }
     }
 })
